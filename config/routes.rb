cd17b2660f8ae9cb17776ce056105cc60540b740
kaito <roots1091@gmail.com>
2020-03-18 01:39:47 +0900

    c

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
 
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
-  post "posts/create" => "posts#create"
   post "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
   post "posts/:id/destroy" => "posts#destroy"
 
   get "/" => "home#top"
