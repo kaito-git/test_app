e3f692a46d73d5242461cce2ed2b5935bd6f7b95
kaito <roots1091@gmail.com>
2020-03-17 12:40:19 +0900

    c

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password]
+    )
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Complete!"
+      redirect_to("/users/#{@user_id}")
+    else
+      render("users/new")
+    end
   end
 
   def login_form
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get "login" => "users#login_form"
 
+  post "users/create" => "users#create"
   get "signup" => "users#new"
+  get "users/:id" => "users#show"
 
   get "/" => "home#top"
 end
